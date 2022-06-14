99d6a6011979973358f87590bbd4fcd6ff565970
Natalia Voronina <nvoronina.dev@gmail.com>
2022-06-14 23:58:39 +0300

    RR playground start

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+include_once '../vendor/autoload.php';
+
+use Spiral\RoadRunner;
+use Nyholm\Psr7;
+
+
+$workerBase = RoadRunner\Worker::create();
+$psrFactory = new Psr7\Factory\Psr17Factory();
+
+$worker = new RoadRunner\Http\PSR7Worker($workerBase, $psrFactory, $psrFactory, $psrFactory);
+
+while ($req = $worker->waitRequest()) {
+//    try {
+        $_COOKIE = $req->getCookieParams() ?? [];
+        $_POST = $req->getParsedBody() ?? [];
+        $_GET = $req->getQueryParams() ?? [];
+        $_REQUEST = array_merge($_POST, $_GET, $_COOKIE);
+
+        $invokedClass = new \App\Public\Controller\Cats($worker, $req, new Psr7\Response());
+        $resp = new Psr7\Response();
+        $method = strtolower($req->getMethod());
+
+//        $worker->respond($resp);
+        $invokedClass->$method();
+//    } catch (\Throwable $e) {
+////        \Slimex\LogRegistry::cliDefaultAndConsoleLogger()->crit($e->getMessage(), \Slimex\CommonHelper::getErrorContext($e));
+////        $worker->getWorker()->error((string)$e);
+//    }
+}
+
---
+++ src/Public/Repository/CatsRepository.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Public\Repository;
+
+class CatsRepository
+{
+    private \PDO $db;
+
+    public function __construct()
+    {
+        $this->db = new \PDO('mysql:host=mysql;dbname=cats', 'user', 'password');
+    }
+
+    public function getAll(): array
+    {
+        return $this
+            ->db
+            ->query('SELECT * FROM cats;')
+            ->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function create(
+        string $name,
+        ?string $type,
+        ?string $description,
+        ?string $specials,
+        ?int $vaccination,
+    ): int
+    {
+        $stm = $this
+            ->db
+            ->prepare('INSERT INTO cats(name, type, description, specials, vaccination) VALUES(?,?,?,?,?)');
+        $stm->execute([$name, $type, $description, $specials, $vaccination]);
+
+        return $this->db->lastInsertId();
+    }
+}
